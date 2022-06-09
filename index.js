73fc93c74d52cfb58b788bf80c17584399a03ed2
Uchkun Ashirov <uchkunashirov@Uchkuns-MacBook-Pro.local>
2022-06-09 18:28:59 +0500

    Node js eski uslubi

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url === '/') {
+      fs.readFile(
+        path.join(__dirname, 'views', 'index.html'),
+        'utf-8',
+        (err, content) => {
+          if (err) {
+            throw err;
+            res.end(content);
+          }
+        }
+      );
+    } else if (req.url === '/contact') {
+      fs.readFile(
+        path.join(__dirname, 'views', 'contact.html'),
+        'utf-8',
+        (err, content) => {
+          if (err) {
+            throw err;
+            res.end(content);
+          }
+        }
+      );
+    }
+  } else if (req.body === 'POST') {
+    const body = [];
+    req.on('data', (data) => {
+      body.push(Buffer.from(data));
+    });
+    req.on('end', () => {
+      console.log(body.toString());
+    });
+    res.end(`<span>Your message: <b></b></span>`);
+  }
+});
+
+server.listen(5000, function () {
+  console.log('Server 5000-portda ishlamoqda...');
+});
---
+++ server.js
@@ -0,0 +1,19 @@
+// const os = require('os');
+// const path = require('path');
+// const fs = require('fs');
+
+// fs.mkdir(path.join(__dirname, 'document'), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log('papka yaratildi');
+// });
+
+// console.log('My file name: ', path.basename(__filename));
+// console.log('My file name: ', path.dirname(__filename));
+// console.log('My file name: ', path.extname(__filename));
+// console.log('My file name: ', path.parse(__filename));
+
+// console.log('my operatsion sistemem: ', os.cpus());
+// const myCpus = os.cpus().forEach((param) => console.log(param.model));
+// console.log(path.join(__dirname, 'index'));
